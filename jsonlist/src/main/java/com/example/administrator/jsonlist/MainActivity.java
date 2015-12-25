9c1bcc9db55963d856acb3a0c50fae0d5669b2e4
xchong <1074974408@qq.com>
2015-12-25 21:19:11 +0800

    可以解析json，但是listview不显示

--- jsonlist/src/main/java/com/example/administrator/jsonlist/MainActivity.java
+++ jsonlist/src/main/java/com/example/administrator/jsonlist/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.administrator.jsonlist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,6 +14,10 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class MainActivity extends Activity {
 
 
@@ -19,17 +25,26 @@ public class MainActivity extends Activity {
     private RequestQueue mQueue;
     private  JsonObjectRequest jsonObjectRequest;
     private static final String WEATHER_LINK = "http://www.weather.com.cn/data/sk/101280101.html";
-
+    private  ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+    private ListView lvWeather;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
 
-         mQueue = Volley.newRequestQueue(this.getApplicationContext());
-         JsonRequst();
+        lvWeather = (ListView)findViewById(R.id.list);
+        mQueue = Volley.newRequestQueue(this);
 
+        JsonRequst();
+
+
+
+        SimpleAdapter adapter = new SimpleAdapter(this,list,R.layout.item,
+                new String[]{"title","info"},
+                new int[]{R.id.t1,R.id.t2});
+        lvWeather.setAdapter(adapter);
 
     }
 
@@ -42,6 +57,32 @@ public class MainActivity extends Activity {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d("TAG", response.toString());
+
+                        try{
+
+                            Iterator<String> it =  response.keys();
+                            while (it.hasNext()){
+
+                                String key = it.next();
+                                String value = response.getString(key);
+                                Log.d("TAG", "title = " + key + " | content = " + value);
+                                HashMap<String, String> map = new HashMap<String, String>();
+                                map.put("title", key);
+                                map.put("content",value);
+
+                                Log.d("TAG", "title = " + key + " | content = " + value);
+
+                                list.add(map);
+
+                            }
+
+                        Log.d("TAG", list.toString());
+
+                        }catch (Exception e){
+
+                           // Log.d("TAG", e.toString());
+                        }
+
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -49,8 +90,9 @@ public class MainActivity extends Activity {
                 Log.e("TAG", error.getMessage(), error);
             }
         });
-
         mQueue.add(jsonObjectRequest);
+
+
     }
 
 }
